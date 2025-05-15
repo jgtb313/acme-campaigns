0dfd011982a42e3f2303d57514d9a9dcef16d8b6
João Tury <jgtb313@gmail.com>
2025-05-14 23:19:54 -0300

    small changes

--- acme-campaigns-frontend/src/ui/navbar/navbar.tsx
+++ acme-campaigns-frontend/src/ui/navbar/navbar.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { Icon2fa, IconLogout } from "@tabler/icons-react";
 
 import classes from "./navbar.module.css";
@@ -8,30 +7,30 @@ export type NavbarItemProps = {
   label: string;
   to: string;
   icon: typeof Icon2fa;
-  active?: boolean;
+  active?: string;
 };
 export type NavbarProps = {
+  active?: string;
   items: NavbarItemProps[];
 };
 
 const NavbarItem = (item: NavbarItemProps) => {
-  const pathname = usePathname();
-  const active = pathname === item.to ? true : undefined;
+  const isActive = item.active === item.to ? true : undefined;
 
   return (
-    <Link className={classes.link} href={item.to} data-active={active}>
+    <Link className={classes.link} href={item.to} data-active={isActive}>
       <item.icon className={classes.linkIcon} stroke={1.5} />
       <span>{item.label}</span>
     </Link>
   );
 };
 
-export const Navbar = ({ items }: NavbarProps) => {
+export const Navbar = ({ items, active }: NavbarProps) => {
   return (
     <nav className={classes.navbar}>
       <div className={classes.navbarMain}>
         {items.map((item) => (
-          <NavbarItem key={item.label} {...item} />
+          <NavbarItem key={item.label} active={active} {...item} />
         ))}
       </div>
 
