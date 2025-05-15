e1f828286be7db56cd094f9fa360ecb1871cfcdd
João Tury <jgtb313@gmail.com>
2025-05-14 23:22:20 -0300

    adding @tanstack/react-query Provider

--- acme-campaigns-frontend/src/layouts/default.layout.tsx
+++ acme-campaigns-frontend/src/layouts/default.layout.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import type { PropsWithChildren } from "react";
+import { usePathname } from "next/navigation";
 import { AppShell } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { IconLayoutDashboard, IconMessages } from "@tabler/icons-react";
 
 import { Header, Navbar, NavbarItemProps } from "~/ui";
@@ -19,22 +21,28 @@ const navbarItems: NavbarItemProps[] = [
   },
 ];
 
+const queryClient = new QueryClient();
+
 export const DefaultLayout = ({ children }: PropsWithChildren) => {
+  const pathname = usePathname();
+
   return (
-    <AppShell
-      header={{ height: 80 }}
-      navbar={{ width: 190, breakpoint: "sm" }}
-      padding="xl"
-    >
-      <AppShell.Header>
-        <Header />
-      </AppShell.Header>
+    <QueryClientProvider client={queryClient}>
+      <AppShell
+        header={{ height: 80 }}
+        navbar={{ width: 190, breakpoint: "sm" }}
+        padding="xl"
+      >
+        <AppShell.Header>
+          <Header />
+        </AppShell.Header>
 
-      <AppShell.Navbar>
-        <Navbar items={navbarItems} />
-      </AppShell.Navbar>
+        <AppShell.Navbar>
+          <Navbar active={pathname} items={navbarItems} />
+        </AppShell.Navbar>
 
-      <AppShell.Main>{children}</AppShell.Main>
-    </AppShell>
+        <AppShell.Main>{children}</AppShell.Main>
+      </AppShell>
+    </QueryClientProvider>
   );
 };
